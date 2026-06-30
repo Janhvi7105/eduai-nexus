575af8d1dd19b0cb4f3f6193511b10c86c0f78f5
Janhvi1705 <jghuikar98@gmail.com>
2026-07-01 00:45:56 +0530

    Add CORS test header

--- backend/app.js
+++ backend/app.js
@@ -13,6 +13,11 @@ app.use(cors({
 }));
 app.use(express.json());
 
+app.use((req, res, next) => {
+  res.setHeader("X-CORS-TEST", "NEW-CORS-CONFIG");
+  next();
+});
+
 app.get("/", (req, res) => {
   res.send("EduAI Nexus API running 🚀");
 });
